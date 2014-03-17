84e912d35da3e753e17c239e68aa6f4dc69a8363
Philipp Borgers <borgers@mi.fu-berlin.de>
2014-03-17 12:43:16 +0100

    initial commit

---
+++ manifests/init.pp
@@ -0,0 +1,48 @@
+class cassandra-cpp {
+
+  #TODO fix this, bad style
+  # move common packages in one module
+  if ! defined(Package['build-essential']) {
+    package { 'build-essential':
+      ensure => installed,
+    }
+  }
+  if ! defined(Package['libssl-dev']) {
+    package { 'libssl-dev':
+      ensure => installed,
+    }
+  }
+  if ! defined(Package['libboost-all-dev']) {
+    package { 'libboost-all-dev':
+      ensure => installed,
+    }
+  }
+
+  package { [
+    'libssh2-1-dev',
+    'cmake'
+    ]:
+      ensure => installed
+  }
+
+  exec { 'git clone https://github.com/datastax/cpp-driver.git /usr/src/cassandra-cpp':
+    path => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+  }
+
+  #TODO build only once
+  exec { 'cmake . && make && make install && ldconfig':
+    path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+    cwd     => '/usr/src/cassandra-cpp',
+    timeout => 900,
+    require => [
+  		Exec['git clone https://github.com/datastax/cpp-driver.git /usr/src/cassandra-cpp'],
+      Package[
+        'build-essential',
+        'libssl-dev',
+        'libboost-all-dev',
+        'libssh2-1-dev',
+        'cmake'
+      ]
+    ]
+  }
+}
